a1776dcf304383b0c2866e8b64671c0c2d079224
Cody Thigpen <ckthigpen1@gmail.com>
2021-07-18 14:26:15 -0400

    added image to project page

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
+import './Projects.css'
 
 function Projects(props) {
     return (
         <div>
             <Navbar />
             <header>
-                <h1>Projects</h1>
-                <p>Check out several of my select projects below. I specialize in React and JavaScript.</p>
+                <div id='project-top'>
+                    <h1>Projects</h1>
+                    <p>Check out several of my select projects below. I specialize in React and JavaScript.</p>
+                </div>
             </header>
         </div>
     );
